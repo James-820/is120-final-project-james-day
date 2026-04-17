c0b670796192d9262d3726f37d7731b30a442143
James Day <daj4102@byu.edu>
2026-04-16 22:54:27 -0600

    index.html, style.css, script.js: Adding user inputs

--- script.js
+++ script.js
@@ -3,8 +3,10 @@
 // ________________________________________________________________________________
 
 let card_container = document.querySelector("#card-container");
-let input_container = document.querySelector("#add-input");
+let input_container = document.querySelector("#input-container");
 let add_btn = document.querySelector("#add-btn");
+let cancel_btn = document.querySelector("#cancel-btn");
+
 let lift_list = localStorage.lift_list;
 
 // Function declarations:
@@ -22,7 +24,51 @@ function makeEmpty() {
   p.textContent = 'To start, click the "Add Lift" button above.';
   placeholder.appendChild(h3);
   placeholder.appendChild(p);
+  placeholder.classList += "flex-column-center";
+  placeholder.style.padding = "1rem";
   return placeholder;
+}
+
+// Make/clear input fields:
+// ________________________________________
+
+function makeInputs() {
+  input_container.innerHTML = "";
+  cancel_btn.style.display = "inline-block";
+  // Add inputs and labels:
+  // Name:
+  let name_container = document.createElement("div");
+  let name_input = document.createElement("input");
+  name_input.type = "text";
+  name_input.placeholder = 'Ex: "Bench Press"';
+  name_input.id = "name-input";
+  let name_label = document.createElement("label");
+  name_label.for = "name-input";
+  name_label.textContent = "Name of lift:";
+  name_container.appendChild(name_label);
+  name_container.appendChild(name_input);
+  // Weight:
+  let weight_container = document.createElement("div");
+  let weight_input = document.createElement("input");
+  weight_input.type = "number";
+  weight_input.placeholder = "Ex: 125";
+  weight_input.id = "weight-input";
+  let weight_label = document.createElement("label");
+  weight_label.for = "weight-input";
+  weight_label.textContent = "Weight (lbs):";
+  weight_container.appendChild(weight_label);
+  weight_container.appendChild(weight_input);
+  // Reps/Sets:
+  // let reps_input = document.createElement("");
+  // let reps_label = document.createElement("");
+  // Append to the inputs container:
+  input_container.appendChild(name_container);
+  input_container.appendChild(weight_container);
+}
+
+function clearInputs() {
+  input_container.innerHTML = "";
+  cancel_btn.style.display = "none";
 }
 
 // Make a lift card:
@@ -45,7 +91,10 @@ function displayAll() {
 // Add new lift object to localstorage array:
 // ________________________________________
 
-function addLift() {}
+function addLift() {
+  // Show the input fields:
+  makeInputs();
+}
 
 // On load:
 // ________________________________________________________________________________
@@ -53,3 +102,5 @@ function addLift() {}
 
 // Display lift cards:
 displayAll();
+makeInputs();
+console.log(input_container);
