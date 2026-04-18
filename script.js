70eb4ca01992ad2a4357963db5059c7e2c850af4
James Day <daj4102@byu.edu>
2026-04-18 17:56:47 -0600

    index.html, style.css, script.js: added Neumorphism, started working with localStorage

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ let input_container = document.querySelector("#input-container");
 let add_btn = document.querySelector("#add-btn");
 let cancel_btn = document.querySelector("#cancel-btn");
 
-let lift_list = localStorage.lift_list;
+if (localStorage.getItem("lift-list") === null) {
+  localStorage.setItem("lift-list", []);
+}
+
+let lift_list = [];
+// Add each to the list:
+for (const obj of JSON.parse(localStorage.getItem("lift-list"))) {
+  lift_list.push(obj);
+}
+let currently_adding = false;
+let name_id = "name-input";
+let weight_id = "weight-input";
+let reps_id = "reps-input";
 
 // Function declarations:
 // ________________________________________________________________________________
@@ -35,13 +47,14 @@ function makeEmpty() {
 function makeInputs() {
   input_container.innerHTML = "";
   cancel_btn.style.display = "inline-block";
+  add_btn.textContent = "Save Changes";
   // Add inputs and labels:
   // Name:
   let name_container = document.createElement("div");
   let name_input = document.createElement("input");
   name_input.type = "text";
   name_input.placeholder = 'Ex: "Bench Press"';
-  name_input.id = "name-input";
+  name_input.id = name_id;
   let name_label = document.createElement("label");
   name_label.for = "name-input";
   name_label.textContent = "Name of lift:";
@@ -52,23 +65,35 @@ function makeInputs() {
   let weight_input = document.createElement("input");
   weight_input.type = "number";
   weight_input.placeholder = "Ex: 125";
-  weight_input.id = "weight-input";
+  weight_input.id = weight_id;
   let weight_label = document.createElement("label");
   weight_label.for = "weight-input";
   weight_label.textContent = "Weight (lbs):";
   weight_container.appendChild(weight_label);
   weight_container.appendChild(weight_input);
   // Reps/Sets:
-  // let reps_input = document.createElement("");
-  // let reps_label = document.createElement("");
+  let reps_container = document.createElement("div");
+  let reps_input = document.createElement("input");
+  reps_input.id = "reps-input";
+  reps_input.placeholder = "Ex: 8, 7, 7";
+  let reps_label = document.createElement("label");
+  reps_label.for = reps_id;
+  reps_label.textContent = "Reps per Set:";
+  reps_container.appendChild(reps_label);
+  reps_container.appendChild(reps_input);
   // Append to the inputs container:
   input_container.appendChild(name_container);
   input_container.appendChild(weight_container);
+  input_container.appendChild(reps_container);
+  input_container.style.padding = "1rem";
 }
 
 function clearInputs() {
   input_container.innerHTML = "";
+  input_container.style.padding = "0";
   cancel_btn.style.display = "none";
+  add_btn.textContent = "Add Lift";
+  currently_adding = false;
 }
 
 // Make a lift card:
@@ -86,14 +111,29 @@ function displayAll() {
     card_container.appendChild(makeEmpty());
     return;
   }
+  console.log(lift_list);
 }
 
 // Add new lift object to localstorage array:
 // ________________________________________
 
 function addLift() {
-  // Show the input fields:
-  makeInputs();
+  // Check if currently adding lift:
+  if (currently_adding) {
+    // Add a lift with the inputs:
+    // Make the lift object:
+    // Append to the list:
+    lift_list.push;
+    // Remove input fields:
+    clearInputs();
+    // Refresh the list display for user:
+    displayAll();
+  } else {
+    // Set adding variable to true:
+    currently_adding = true;
+    // Show inputs:
+    makeInputs();
+  }
 }
 
 // On load:
@@ -102,5 +142,11 @@ function addLift() {
 
 // Display lift cards:
 displayAll();
-makeInputs();
-console.log(input_container);
+add_btn.addEventListener("click", addLift);
+cancel_btn.addEventListener("click", clearInputs);
+// cancel_btn.addEventListener("click", () => {
+//   currently_adding = false;
+//   addLift();
+// });
+// makeInputs();
+// console.log(input_container);
